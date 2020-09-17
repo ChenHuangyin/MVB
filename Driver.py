8e2d6692f54c78dcbda30eeac1f137f84f3fad68
Squarepants <chenhy98@mail2.sysu.edu.cn>
2020-09-17 03:28:30 -0400

    Update "doubleSpendTest" func

--- Driver.py
+++ Driver.py
@@ -4,9 +4,11 @@ from Blockchain.Block import *
 from Blockchain.Node import *
 
 if __name__ == '__main__':
-    Test1 = MVBTest(8, 15)
-    node1Json = Test1.mvb.networkNodes[0].getJson()
-    with open('node1.json', 'w', encoding='utf-8') as f:
-        f.write(node1Json)
-    print(node1Json)
+    Test1 = MVBTest(1, 15)  # MVBTest(nodes cnt, Keys cnt)
+    Test1.doubleSpendTest()
+
+    # node1Json = Test1.mvb.networkNodes[0].getJson()
+    # with open('node1.json', 'w', encoding='utf-8') as f:
+    #     f.write(node1Json)
+    # print(node1Json)
 
